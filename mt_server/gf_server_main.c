d88af80e4b96f7504d950df6fa12c85614dfe48c
Andrew Wei <awei34@gatech.edu>
2020-05-10 21:27:32 -0700

    mt working

---
+++ mt_server/gf_server_main.c
@@ -0,0 +1,164 @@
+#include <getopt.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <dirent.h>
+#include <sys/signal.h>
+
+#include <pthread.h>
+
+#include "http_server.h"
+#include "gf_handler.h"
+
+
+#define USAGE                                                                 \
+"usage:\n"                                                                    \
+"  http_server_main [options]\n"                                              \
+"options:\n"                                                                  \
+"  -p [listen_port]    Listen port (Default: 8888)\n"                         \
+"  -b [bind_address]   Address to bind to (Default: 0.0.0.0)\n"               \
+"  -d [directory]      Alternate directory (Default: current directory)\n"    \
+"  -t [nthreads]       Number of threads (Default: 4)\n"    \
+"  -h                  Show this help message.\n"                             \
+
+/* OPTIONS DESCRIPTOR ====================================================== */
+static struct option gLongOptions[] = {
+{"port",          required_argument,      NULL,           'p'},
+{"bind_address",  required_argument,      NULL,           'b'},
+{"directory",     required_argument,      NULL,           'd'},
+{"nthreads",      optional_argument,      NULL,           't'},
+{"help",          no_argument,            NULL,           'h'},
+{NULL,            0,                      NULL,             0}
+};
+
+#define PATHLIM 1024
+
+static void _sig_handler(int signo){
+  if (signo == SIGINT || signo == SIGTERM){
+    exit(signo);
+  }
+}
+
+/* Main ========================================================= */
+int main(int argc, char **argv) {
+    int option_char = 0;
+    unsigned short portnum = 6200;
+    char ip_addr[PATHLIM];
+    strncpy(ip_addr, "0.0.0.0", PATHLIM);
+
+    char directory[PATHLIM];
+    strncpy(directory, ".", PATHLIM);
+
+    int nthreads = 4;
+
+    setbuf(stdout, NULL); // disable caching
+
+    if (signal(SIGINT, _sig_handler) == SIG_ERR){
+        fprintf(stderr,"Can't catch SIGINT...exiting.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (signal(SIGTERM, _sig_handler) == SIG_ERR){
+        fprintf(stderr,"Can't catch SIGTERM...exiting.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Parse and set command line arguments
+    while ((option_char = getopt_long(argc, argv, "p:b:d:hx", gLongOptions, NULL)) != -1) {
+        switch (option_char) {
+            case 'p': // listen-port
+                portnum = atoi(optarg);
+                if ((portnum < 1025) || (portnum > 65535)) {
+                fprintf(stderr, "%s @ %d: invalid port number (%d)\n", __FILE__, __LINE__, portnum);
+                exit(1);
+                } 
+                break;
+            case 'b': // bind address
+                if (strlen(optarg) >= PATHLIM/4) {
+                    fprintf(stdout, "invalid bind address");
+                    exit(1);
+                }
+                strncpy(ip_addr, optarg, PATHLIM);
+
+                break;
+            case 'd':
+                // limit directory path to 256 chars
+                if (strlen(optarg) >= PATHLIM/4) {
+                    fprintf(stdout, "directory path over %d limit", PATHLIM/4);
+                    exit(1);
+                }
+                strncpy(directory, optarg, PATHLIM);
+
+                // check directory exists
+                DIR* dir = opendir(directory);
+                if (dir) {                      /* Directory exists. */
+                    closedir(dir);
+                } else if (ENOENT == errno) {   /* Directory does not exist. */
+                    fprintf(stdout, "directory %s does not exist\n", directory);
+                    exit(0);
+                } else {                        /* opendir() failed for some other reason. */
+                    fprintf(stdout, "opendir() error\n");
+                    exit(1);
+                }
+
+                break;
+            case 't': // nthreads
+                nthreads = atoi(optarg);
+                if (nthreads <= 1) {
+                    nthreads = 1;
+                }
+                break;
+            case 'h': // help
+                fprintf(stdout, "%s", USAGE);
+                exit(0);
+                break;       
+            default:
+                fprintf(stderr, "%s", USAGE);
+                exit(1);                                       
+        }
+    }
+
+    // set up GET FILE handler
+    http_handler_t *gf_handler = gf_handler_create();
+    gf_handler_set_dir(gf_handler, directory);
+
+
+    /*Initialize server*/
+    http_server_t *getfile_server = http_server_create();
+
+    /*Setting options*/
+    http_server_set_port(getfile_server, portnum);
+    http_server_set_address(getfile_server, ip_addr);
+    http_server_set_maxpending(getfile_server, 6);
+    http_server_set_handler(getfile_server, gf_handler);
+
+    // www.cs.cornell.edu/courses/cs2022/2011sp/lectures/lect11.pdf
+    pthread_t threads[nthreads];
+    worker_args_t *worker_args[nthreads];
+    long t;
+    int rc;
+    for (t = 0; t < nthreads; t++) {
+        //printf("# In main: creating thread %ld\n", t);
+        
+        worker_args[t] = (worker_args_t *) malloc(sizeof(worker_args_t)*nthreads);
+        worker_args[t]->thread_id = t;
+        worker_args[t]->handler = gf_handler;
+
+        rc = pthread_create(&threads[t], NULL, http_server_worker, (void *) worker_args[t]);
+        if (rc) {
+        printf("ERROR; return code from pthread_create() is %d\n", rc);
+        } 
+    }
+
+    /*Loops forever*/
+    http_server_serve(getfile_server);
+    free(getfile_server);
+}
---
+++ mt_server/http_server.c
@@ -0,0 +1,186 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+
+#include "http_server.h"
+#include "gf_handler.h"
+
+// FIFO queue implementation
+int client_fd_queue[QUEUESIZE]; // QUEUESIZE defined in "gfsesrver-student.h"
+char request_queue[QUEUESIZE][PATHLIM];
+int queueBack = 0; // queueBack tracks where handler will insert to queue
+int queueFront = 0; // queueFront tracks where workers will remove from queue
+int queueCount = 0; // queueCount tracks current # of items in queue
+
+pthread_mutex_t queue_mutex = PTHREAD_MUTEX_INITIALIZER;	
+// condition var to control handler adding to the queues
+pthread_cond_t cv_in = PTHREAD_COND_INITIALIZER;
+// condition var to control workers removing from queues
+pthread_cond_t cv_out = PTHREAD_COND_INITIALIZER;
+
+
+
+typedef struct http_server_t {
+    struct sockaddr_in serv_addr;
+    int maxpending;
+    http_handler_t *handler;
+    void* handlerarg;
+} http_server_t;
+
+
+http_server_t* http_server_create(){
+    http_server_t *server = (http_server_t *)malloc(sizeof(http_server_t));
+    memset(&(server->serv_addr), 0, sizeof(server->serv_addr));
+
+    server->serv_addr.sin_family = AF_INET;
+    server->serv_addr.sin_addr.s_addr = INADDR_ANY;
+    server->serv_addr.sin_port = htons(6200);
+
+    server->maxpending = 1;
+    server->handler = NULL;
+    return server;
+}
+
+void http_server_set_port(http_server_t *server, unsigned short port) {
+    server->serv_addr.sin_port = htons(port);
+}
+
+void http_server_set_address(http_server_t *server, char* serv_addr) {
+    inet_pton(server->serv_addr.sin_family, serv_addr, &(server->serv_addr.sin_addr));
+}
+
+void http_server_set_maxpending(http_server_t *server, int max_npending) {
+    server->maxpending = max_npending;
+}
+
+void http_server_set_handler(http_server_t *server, http_handler_t *handler) {
+    server->handler = handler;
+}
+
+
+void http_server_serve(http_server_t *server){
+    char buf[BUFSIZE];
+    memset(buf, 0, BUFSIZE);
+
+    // set up listening port
+    int listenerfd, newsockfd;
+    socklen_t clilen;
+
+    listenerfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (listenerfd < 0)
+        perror("ERROR opening socket");
+
+    int yes = 1;
+    setsockopt(listenerfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
+    
+    if (bind(listenerfd, (struct sockaddr *) &server->serv_addr, sizeof(server->serv_addr)) < 0)
+        perror("ERROR on binding");
+    printf("# Listener socket %d bound to %s:%d.\n", listenerfd, inet_ntoa(server->serv_addr.sin_addr) , ntohs(server->serv_addr.sin_port));
+
+    printf("# Server is active. Listening for connections...\n");
+    if (listen(listenerfd, server->maxpending) == -1) {
+        perror("listen");
+        exit(3);
+    }
+
+    struct sockaddr_in cli_addr;
+    clilen = sizeof cli_addr; 
+
+    // serve
+    while(1) {
+        // ACCEPT
+        newsockfd = accept(listenerfd, (struct sockaddr *) &cli_addr, &clilen);
+        if (newsockfd < 0) {
+            perror("ERROR on accept");
+            continue;
+        }
+        char *clientIP;
+        clientIP = inet_ntoa(cli_addr.sin_addr);
+        printf("- Accepted a connection from %s on socket %d\n", clientIP, newsockfd);
+
+        // Receive the request
+        memset(buf, 0, BUFSIZE);
+        int nbytes;
+        nbytes = recv(newsockfd, buf, BUFSIZE, 0);
+        if (nbytes == 0) {
+            printf("socket %d hung up\n", newsockfd);
+            close(newsockfd);
+            continue;
+        } else if (nbytes < 0) {
+            perror("recv");
+            close(newsockfd);
+            continue;
+        }
+        printf("received request: %s\n", strtok(buf, "\n"));
+
+        /* handle the request - multithreaded */
+        // get lock, wait for queue to be available
+        pthread_mutex_lock(&queue_mutex);
+        while (queueCount >= QUEUESIZE) {
+            printf("### queue maxed. handler waiting...\n");
+            pthread_cond_wait(&cv_in, &queue_mutex);
+        }
+        queueCount++;
+
+        client_fd_queue[queueBack] = newsockfd;
+	    strncpy(request_queue[queueBack], buf, PATHLIM);
+
+        // increment queueBack pointer
+        if (queueBack == (QUEUESIZE - 1)) {
+            queueBack = 0;
+        } else {
+            queueBack ++;
+        }
+        // release lock
+        pthread_mutex_unlock(&queue_mutex);
+        pthread_cond_signal(&cv_out);
+
+
+        //server->handler->handle(server->handler, buf, newsockfd);
+    }
+}
+
+void *http_server_worker(void *args) {
+    //long tid = ((worker_args_t *)args)->thread_id;
+  	//printf("# Thread #%ld created.\n", tid);
+
+    http_handler_t *handler = ((worker_args_t *)args)->handler;
+    while (1) {
+        // lock and wait for a request on the queue
+        pthread_mutex_lock(&queue_mutex);
+		while (queueCount == 0) {
+			//printf("# Thread #%ld: queueCount = 0. Waiting for work...\n", tid);
+			pthread_cond_wait(&cv_out, &queue_mutex);
+		}
+		queueCount--;
+
+        int client_fd;
+		char *requestbuf;
+		client_fd = client_fd_queue[queueFront];
+		requestbuf = request_queue[queueFront];
+
+        //printf("%d %d %s %lu\n", queueBack, client_fd, requestbuf, strlen(requestbuf));
+
+        // point queueFront to next queue position
+		if (queueFront == (QUEUESIZE-1)) {
+			queueFront = 0;
+		} else {
+			queueFront ++;
+		}
+		pthread_mutex_unlock(&queue_mutex);
+		pthread_cond_signal(&cv_in);
+
+        // do work
+        handler->handle(handler, requestbuf, client_fd);
+
+    }
+}
